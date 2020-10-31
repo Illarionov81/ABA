02b63fd87634213edbf36747314258d4bf366160
Artur <metis130383@gmail.com>
2020-11-01 00:15:00 +0600

    Создал приложение   Webapp

---
+++ source/webapp/models.py
@@ -0,0 +1,308 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+PROGRAM_STATUS_OPEN = 'open'
+PROGRAM_STATUS_CLOSED = 'closed'
+PROGRAM_STATUS_CHOICES = (
+    (PROGRAM_STATUS_OPEN, 'Открыта'),
+    (PROGRAM_STATUS_CLOSED, 'Закрыта')
+)
+
+SESSION_STATUS_OPEN = 'open'
+SESSION_STATUS_CLOSED = 'closed'
+SESSION_STATUS_CHOICES = (
+    (SESSION_STATUS_OPEN, 'Открыта'),
+    (SESSION_STATUS_CLOSED, 'Закрыта')
+)
+
+SKILL_STATUS_OPEN = 'open'
+SKILL_STATUS_CLOSED = 'closed'
+SKILL_STATUS_CHOICES = (
+    (SKILL_STATUS_OPEN, 'Открыт'),
+    (SKILL_STATUS_CLOSED, 'Закрыт')
+)
+
+
+class SoftDeleteManager(models.Manager):
+    def active(self):
+        return self.filter(is_deleted=False)
+
+    def deleted(self):
+        return self.filter(is_deleted=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='profile', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, null=False, blank=False, verbose_name='Телефон пользователя')
+    children = models.ManyToManyField('Child', through='Therapy', blank=True, related_name='therapists',
+                                      verbose_name='Ребенок')
+
+    def __str__(self):
+        return "%s" % self.user.username
+
+
+class Child(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name='Имя ребенка')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия ребенка', blank=True, null=True)
+    third_name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Отчество ребенка')
+    birthday = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    age = models.CharField(max_length=100, verbose_name='Возраст')
+    address = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Адрес проживания')
+    characteristic = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Характеристика на ребенка')
+    preferences = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Предпочтения ребенка')
+    first_parent = models.CharField(max_length=255, verbose_name='Родитель')
+    second_parent = models.CharField(max_length=255, blank=True, null=True, verbose_name='Второй родитель')
+    contacts = models.CharField(max_length=200, blank=True, null=True, verbose_name='Контакты ребенка')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления ребенка')
+    edited_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name='Дата редактирования')
+    deleted_date = models.DateTimeField(null=True, blank=True, verbose_name='Дата удаления')
+    communication_system = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Система коммуникации')
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+    class Meta:
+        verbose_name = 'Дети'
+        verbose_name_plural = 'Дети'
+
+
+class Therapy(models.Model):
+    user = models.ForeignKey(UserProfile, related_name='related_to_user', on_delete=models.CASCADE,
+                             verbose_name='Терапист')
+    child = models.ForeignKey(Child, null=True, blank=True, related_name='child_in_user', on_delete=models.CASCADE,
+                              verbose_name='Ребенок')
+
+    def __str__(self):
+        return self.user.user.username
+
+    class Meta:
+        verbose_name = 'Дети пользователя'
+        verbose_name_plural = 'Дети пользователей'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название секции')
+    code = models.CharField(max_length=5, verbose_name='Код секции')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления')
+
+    def __str__(self):
+        return "%s. %s" % (self.code, self.name)
+
+    class Meta:
+        verbose_name = 'Секция'
+        verbose_name_plural = 'Секции'
+
+
+
+
+class Skill(models.Model):
+    code = models.CharField(max_length=5, verbose_name='Код навыка')
+    name = models.CharField(max_length=255, verbose_name='Название навыка')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='skills', verbose_name='Секция')
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание навыка')
+    max_level = models.PositiveSmallIntegerField(default=1, verbose_name="Количество уровней сложности")
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def __str__(self):
+        return "%s. %s" % (self.code, self.name)
+
+    class Meta:
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
+
+
+class SkillLevel(models.Model):
+    skill = models.ForeignKey('Skill', on_delete=models.CASCADE, related_name='levels', verbose_name='Навык')
+    level = models.IntegerField(verbose_name='Уровень')
+    criteria = models.CharField(max_length=1000, null=True, blank=True, verbose_name='Критерии')
+
+    def __str__(self):
+        return "{} - {} | {}".format(self.skill.code, self.level, self.skill.name)
+
+    class Meta:
+        verbose_name = 'Уровень навыка'
+        verbose_name_plural = 'Уровни навыков'
+        ordering = ('skill', 'level')
+
+
+class Program(models.Model):
+    child = models.ForeignKey('Child', on_delete=models.PROTECT, related_name='programs',
+                              verbose_name='Ребенок')
+    author = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name='programs',
+                               verbose_name='Автор')
+    start_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(verbose_name="Дата окончания")
+    skills = models.ManyToManyField('SkillLevel', verbose_name='Навыки', related_name='in_programs', blank=True)
+    status = models.CharField(max_length=20, choices=PROGRAM_STATUS_CHOICES, default=PROGRAM_STATUS_OPEN,
+                              verbose_name='Статус')
+    name = models.CharField(max_length=100, null=True, blank=True, verbose_name="Название программы")
+    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name="Описание программы")
+    comment = models.CharField(max_length=255, null=True, blank=True, verbose_name='Комментарий к программе')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    edited_date = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Дата редактирования")
+    deleted_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата удаления")
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def is_open(self):
+        return self.status == PROGRAM_STATUS_OPEN
+
+    def __str__(self):
+        return "%s %s" % (self.child, self.created_date)
+
+    class Meta:
+        verbose_name = 'Программа'
+        verbose_name_plural = 'Программы'
+
+
+class Session(models.Model):
+    child = models.ForeignKey('Child', related_name='sessions', blank=True, on_delete=models.PROTECT,
+                              verbose_name='Ребёнок')
+    program = models.ForeignKey('Program', related_name='sessions', null=True, blank=True, on_delete=models.PROTECT,
+                                verbose_name='Программа')
+    therapist = models.ForeignKey('UserProfile', on_delete=models.PROTECT, blank=True, related_name='sessions')
+    comment = models.TextField(max_length=1000, blank=True, null=True, verbose_name="Комментарий")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    edited_date = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Дата редактирования")
+    deleted_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата удаления")
+    status = models.CharField(max_length=20, choices=SESSION_STATUS_CHOICES, default=SESSION_STATUS_OPEN,
+                              verbose_name='Статус')
+
+
+
+
+    def __str__(self):
+        return "{} {} ({})".format(self.child, self.created_date.strftime('%d.%m.%Y'), self.therapist)
+
+    class Meta:
+        verbose_name = 'Сессия'
+        verbose_name_plural = 'Сессии'
+
+
+class SessionSkill(models.Model):
+    session = models.ForeignKey('Session', related_name='skills', on_delete=models.CASCADE, verbose_name='Сессия')
+    skill = models.ForeignKey('Skill', related_name='in_sessions', on_delete=models.CASCADE, verbose_name='Навык')
+    stimulus = models.TextField(max_length=200, null=True, blank=True, verbose_name='Стимул')
+    parent_skill = models.ForeignKey('SessionSkill', related_name='dependant_skills', on_delete=models.SET_NULL,
+                                     null=True, blank=True, verbose_name='Поднавык для')
+    status = models.CharField(max_length=20, choices=SKILL_STATUS_CHOICES, default=SKILL_STATUS_OPEN,
+                              verbose_name="Статус")
+    done_self = models.PositiveSmallIntegerField(default=0, verbose_name="Самостоятельные реакции")
+    done_with_hint = models.PositiveSmallIntegerField(default=0, verbose_name="Реакции с подсказкой")
+
+    def __str__(self):
+        return "{} ({})".format(self.skill, self.stimulus)
+
+    @property
+    def total(self):
+        return self.done_self + self.done_with_hint
+
+    @property
+    def success_percent(self):
+        if self.total == 0:
+            return 0
+        return self.done_self / self.total * 100
+
+    class Meta:
+        verbose_name = 'Навыки для отработки'
+        verbose_name_plural = 'Навыки для отработки'
+        unique_together = ('skill', 'stimulus')
+
+
+class SessionSkillExtras(models.Model):
+    session_skill = models.OneToOneField('SessionSkill', on_delete=models.CASCADE, related_name='extras',
+                                         verbose_name='Сессия')
+    mastery_criterion = models.CharField(max_length=255, null=True, blank=True, verbose_name='Критерий мастерства')
+    study_method = models.ForeignKey('StudyMethod', on_delete=models.SET_NULL, null=True, blank=True,
+                                     verbose_name='Метод обучения')
+    reinforcement_reaction = models.CharField(max_length=255, null=True, blank=True,
+                                              verbose_name='Реакция для подкрепления')
+    reinforcement_mode = models.CharField(max_length=255, null=True, blank=True, verbose_name='Режим подкрепления')
+    hint_type = models.ForeignKey('HintType', on_delete=models.SET_NULL, null=True, blank=True,
+                                  verbose_name='Вид подсказки')
+    hint_type_delete = models.ForeignKey('HintTypeDelete', on_delete=models.SET_NULL, null=True, blank=True,
+                                         verbose_name='Способ удаления подсказки')
+    hint_levels = models.CharField(max_length=255, null=True, blank=True, verbose_name='Уровень подсказки')
+    generalization = models.CharField(max_length=255, null=True, blank=True, verbose_name='Обобщение')
+    reaction_generalization = models.CharField(max_length=255, null=True, blank=True, verbose_name='Обобщение реакции')
+    stimulus_generalization = models.CharField(max_length=255, null=True, blank=True, verbose_name='Обобщение стимула')
+
+    def __str__(self):
+        return str(self.session_skill)
+
+    class Meta:
+        verbose_name = 'Детали отработки'
+        verbose_name_plural = 'Детали отработки'
+
+
+class Test(models.Model):
+    child = models.ForeignKey('Child', on_delete=models.PROTECT, related_name='child_test',
+                              verbose_name='Ребенок')
+    attending_therapist = models.ForeignKey(UserProfile, on_delete=models.PROTECT, null=True,
+                                            related_name='attending_test')
+    status = models.BooleanField(default=True, verbose_name='Статус')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    edited_date = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Дата редактирования")
+    deleted_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата удаления")
+
+
+
+
+    class Meta:
+        verbose_name = 'Первоначальное тестирование'
+        verbose_name_plural = 'Первоначальное тестирование'
+
+
+class TestResult(models.Model):
+    test = models.ForeignKey('Test', on_delete=models.PROTECT, related_name='test_result',
+                             verbose_name='Тест')
+    skill = models.ForeignKey('Skill', on_delete=models.PROTECT, related_name='test_skill',
+                              verbose_name='Навык')
+    level = models.PositiveSmallIntegerField(default=0, verbose_name="Уровень владения навыком")
+    status = models.BooleanField(default=True, verbose_name='Статус')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    edited_date = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Дата редактирования")
+    deleted_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата удаления")
+
+    class Meta:
+        verbose_name = 'Результаты первоначального тестирования'
+        verbose_name_plural = 'Результаты первоначального тестирования'
+
+
+class StudyMethod(models.Model):
+    study_method = models.CharField(max_length=255, null=True, blank=True, verbose_name='Метод обучения')
+
+    def __str__(self):
+        return "%s" % self.study_method
+
+    class Meta:
+        verbose_name = 'Метод обучения'
+        verbose_name_plural = 'Методы обучения'
+
+
+class HintType(models.Model):
+    hint_type = models.CharField(max_length=255, null=True, blank=True, verbose_name='Вид подсказки')
+
+    def __str__(self):
+        return "%s" % self.hint_type
+
+    class Meta:
+        verbose_name = 'Вид подсказки'
+        verbose_name_plural = 'Виды подсказок'
+
+
+class HintTypeDelete(models.Model):
+    hint_type_delete = models.CharField(max_length=255, null=True, blank=True, verbose_name='Способ удаления подсказки')
+
+    def __str__(self):
+        return "%s" % self.hint_type_delete
+
+    class Meta:
+        verbose_name = 'Метод удаления подсказки'
+        verbose_name_plural = 'Методы удаления подсказки'
