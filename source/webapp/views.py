898898074d4860225d71d961eefcff30259a1ee6
Artur <metis130383@gmail.com>
2020-11-02 12:47:47 +0600

    Добавлена вьюшка  для  детального просмотра

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from webapp.models import Child
 
-# Create your views here.
+
+class ChildDetailView(DetailView):
+    template_name = 'child/child_view.html'
+    model = Child
