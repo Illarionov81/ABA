babe790c3571bd0a0d32477ae1a379313af9844f
Evgenii <neroznik@yandex.ru>
2020-11-06 18:04:18 +0600

    Добавил base шаблон + шаблоны prtial +  изменил  URL (промежуточный результат - будет еще изменено)

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from webapp.views import ChildDetailView, ChildTestsView
+from webapp.views import ChildDetailView, ChildTestsView, IndexView
 
 app_name = 'webapp'
 
 
 urlpatterns = [
-
+    path('', IndexView.as_view(), name='index'),
     path('child/', include([
         path('<int:pk>/', include([
             path('', ChildDetailView.as_view(), name='child_view'),
