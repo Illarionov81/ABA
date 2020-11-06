58b1cf13d97c6fbfe659821e62cfde3cc2d2cf28
xislam <nurdinovislam4@gmail.com>
2020-11-06 19:18:11 +0600

    Сделат тест и остольное было сдеано не сегодня а в тот же день до тикета не помнь тоно , мы обсуждать  бд

--- source/main/urls.py
+++ source/main/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
