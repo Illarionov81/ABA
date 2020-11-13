32ecefe635d044c4a460bceba023a6615353dafb
alex <nuvorish81@mail.ru>
2020-11-13 17:47:16 +0600

    Правки к просмотру теста. Тикет #36

--- source/webapp/context_for_test.py
+++ source/webapp/context_for_test.py
@@ -1,43 +1,43 @@
-from webapp.models import Test
+from webapp.models import Test, SkillLevel
 
 
 class ContextForTest:
     def all_test(self, pk, category_cod):
+        skilllevel = SkillLevel.objects.filter(skill__category__code=category_cod)
         category_cod = category_cod
         test = Test.objects.get(pk=pk)
-        this_test = {}
-        previous_test = []
-        tests = {}
-        all_code = []
+        all_filtered_skill_code = {}
+        numbers_of_levels = []
+        for skl in skilllevel:
+            numbers_of_levels.append(skl.skill.code)
+
+        for s in skilllevel:
+            count = 0
+            for i in numbers_of_levels:
+                if i == s.skill.code:
+                    count += 1
+            all_filtered_skill_code[s.skill.code] = {'previous': [], 'last': '', 'max': count, 'empty': range(count)}
         skill_level = test.skill_level.filter(skill__category__code=category_cod)
-        for s in skill_level:
-            if s.skill.code not in this_test:
-                this_test[s.skill.code] = []
-                for key, value in this_test.items():
-                    if s.skill.code == key and s.level not in value:
-                        this_test[s.skill.code].append(s.level)
+
         if test.previus_test:
             previous_tests = Test.objects.filter(pk__lt=test.pk)
-            for t in previous_tests:
-                for skill_level in t.skill_level.filter(skill__category__code=category_cod):
-                    if skill_level not in previous_test:
-                        previous_test.append(skill_level)
-            for i in previous_test:
-                if i.skill.code not in tests:
-                    tests[i.skill.code] = []
-            for i in previous_test:
-                for key, value in tests.items():
-                    if i.skill.code == key and i.level not in value:
-                        tests[i.skill.code].append(i.level)
-        diff = {}
-        for k, v in tests.items():
-            all_code.append(k)
-        for i, j in this_test.items():
-            if i in all_code:
-                pass
-            else:
-                diff[i] = j
-        print(diff)
-        print(tests)
-        print(this_test)
-        return tests, this_test, diff
+            for key, value in all_filtered_skill_code.items():
+                for t in previous_tests:
+                    for s_l in t.skill_level.filter(skill__category__code=category_cod):
+                        if s_l.skill.code == key:
+                            m = all_filtered_skill_code[key]
+                            for i, j in m.items():
+                                if i == 'previous':
+                                    j.append(s_l.level)
+                                    d = all_filtered_skill_code[key]['max'] - s_l.level
+                                    all_filtered_skill_code[key]['empty'] = range(d)
+
+        for key, value in all_filtered_skill_code.items():
+            for s in skill_level:
+                if s.skill.code == key:
+                    all_filtered_skill_code[key]['last'] = s.level
+                    d = all_filtered_skill_code[key]['max'] - s.level
+                    all_filtered_skill_code[key]['empty'] = range(d)
+
+        print(all_filtered_skill_code)
+        return all_filtered_skill_code
