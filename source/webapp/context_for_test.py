96691438b101090d9fed6c69c8e8496df935487d
alex <nuvorish81@mail.ru>
2020-11-14 23:22:32 +0600

    Правки Тикет #36 1. Выводить клеточки всех уровней навыка. 2. Цвета отличающие результаты предыдущих тестов и выбранного сделать более контрастными. 3. У клеточек не отмеченных уровней навыков тоже выводить номер уровня.

--- source/webapp/context_for_test.py
+++ source/webapp/context_for_test.py
@@ -8,6 +8,7 @@ class ContextForTest:
         test = Test.objects.get(pk=pk)
         all_filtered_skill_code = {}
         numbers_of_levels = []
+
         for skl in skilllevel:
             numbers_of_levels.append(skl.skill.code)
 
@@ -16,7 +17,9 @@ class ContextForTest:
             for i in numbers_of_levels:
                 if i == s.skill.code:
                     count += 1
-            all_filtered_skill_code[s.skill.code] = {'previous': [], 'last': '', 'max': count, 'empty': range(count)}
+            all_filtered_skill_code[s.skill.code] = {'previous': [], 'last': 0, 'max': count,
+                                                     'empty': range(1, count+1), 'max_prev_lev': 0}
+
         skill_level = test.skill_level.filter(skill__category__code=category_cod)
 
         if test.previus_test:
@@ -29,15 +32,27 @@ class ContextForTest:
                             for i, j in m.items():
                                 if i == 'previous':
                                     j.append(s_l.level)
-                                    d = all_filtered_skill_code[key]['max'] - s_l.level
-                                    all_filtered_skill_code[key]['empty'] = range(d)
+                            d = all_filtered_skill_code[key]['max'] - s_l.level
+                            all_filtered_skill_code[key]['empty'] = range(s_l.level+1, all_filtered_skill_code[key]['max']+1)
+                last = all_filtered_skill_code[key]['previous'][-1:]
+                for i in last:
+                    all_filtered_skill_code[key]['previous'] = range(1, i+1)
+                    all_filtered_skill_code[key]['max_prev_lev'] = i
 
         for key, value in all_filtered_skill_code.items():
             for s in skill_level:
                 if s.skill.code == key:
-                    all_filtered_skill_code[key]['last'] = s.level
-                    d = all_filtered_skill_code[key]['max'] - s.level
-                    all_filtered_skill_code[key]['empty'] = range(d)
+                    if all_filtered_skill_code[key]['max_prev_lev'] == s.level or all_filtered_skill_code[key]['max_prev_lev'] > s.level:
+                        pass
+                    else:
+                        if all_filtered_skill_code[key]['max_prev_lev'] > 0:
+                            all_filtered_skill_code[key]['last'] = range(all_filtered_skill_code[key]['max_prev_lev'] + 1, s.level + 1)
+                            d = all_filtered_skill_code[key]['max'] - s.level
+                            all_filtered_skill_code[key]['empty'] = range(s.level+1, all_filtered_skill_code[key]['max']+1)
+                        else:
+                            all_filtered_skill_code[key]['last'] = range(1, s.level + 1)
+                            d = all_filtered_skill_code[key]['max'] - s.level
+                            all_filtered_skill_code[key]['empty'] = range(s.level+1, all_filtered_skill_code[key]['max']+1)
 
         print(all_filtered_skill_code)
         return all_filtered_skill_code
