b83c270c30ceb9d7d18a73c1023fabca12969551
alex <nuvorish81@mail.ru>
2020-11-27 18:35:00 +0600

    Написан тест для проверки функции get_sorted_skill_level класса ContextForTest() ticket #47

--- source/webapp/tests.py
+++ source/webapp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from webapp.context_for_test import ContextForTest
+
+
+class LookTestResultTest(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_get_skill_level(self):
+        c = ContextForTest()
+        category_cod = 'A'
+        all_sorted_skill = c.get_sorted_skill_level(category_cod=category_cod)
+        for skill_level in all_sorted_skill:
+            self.assertEqual(skill_level.skill.code, category_cod)
