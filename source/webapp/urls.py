50a07ecd34b2a1d71f888eebee442441e2e44af3
Emil <emilrakaev@gmail.com>
2020-12-01 18:22:19 +0600

    Реализовано создание и проведение теста

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
-from webapp.views.child_test import TestResultView, TestsView
+from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.child import IndexView, ChildDetailView
-from webapp.views.program import ProgramDetailView
+from webapp.views.program import ProgramDetailView, ProgramCreateView
 
 app_name = 'webapp'
 
@@ -10,11 +10,14 @@ app_name = 'webapp'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
+    path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
+    path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
     path('child/', include([
         path('<int:pk>/', include([
             path('', ChildDetailView.as_view(), name='child_view'),
             path('tests/', TestsView.as_view(), name='child_tests'),
-            path('program/', ProgramDetailView.as_view(), name='program_detail'),
+            path('program/create/', ProgramCreateView.as_view(), name='program_create'),
+            path('create_test/', ChildMakeTestView.as_view(), name='child_create_test'),
         ])),
     ])),
 ]
--- source/webapp/views/child_test.py
+++ source/webapp/views/child_test.py
@@ -1,8 +1,11 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
+import json
+
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
+from django.views.generic import ListView, TemplateView
 
 from webapp.context_for_test import ContextForTest
-from webapp.models import Test
+from webapp.models import Test, Child, Skill, SkillLevel
 
 
 class TestsView(ListView):
@@ -13,7 +16,7 @@ class TestsView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        test = Test.objects.filter(child_id=self.kwargs.get('pk'))
+        test = Test.objects.filter(child_id=self.kwargs.get('pk')).order_by('-id')
         context['Test'] = test
         context['child_pk'] = self.kwargs.get('pk')
         return context
@@ -39,3 +42,49 @@ class TestResultView(ListView):
         all_filtered_skill_code = data
         context['all_filtered_skill_code'] = all_filtered_skill_code
         return context
+
+class ChildMakeTestView(View):
+    def get(self, request, *args, **kwargs):
+        child = get_object_or_404(Child, pk=kwargs.get('pk'))
+        test = Test.objects.create(child=child, therapist=self.request.user)
+        query_tests = child.test.order_by('-created_date')
+        if len(query_tests) > 1:
+            previous_test = query_tests[1]
+            test.previus_test = previous_test
+            test.save()
+        return redirect('webapp:child_update_test', pk=test.pk)
+
+
+class ChildTestUpdateView(TemplateView):
+    template_name = 'child/child_make_test.html'
+    model = Test
+    paginate_by = 5
+    paginate_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        test = get_object_or_404(Test, pk=self.kwargs.get('pk'))
+        context = super().get_context_data(**kwargs)
+        category_cod = self.request.GET.get('ABC')
+        context['skills'] = Skill.objects.filter(category__code=category_cod)
+        context['child_pk'] = test.child.pk
+        context['test_pk'] = test.pk
+        context['ABC'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        return context
+
+    def post(self, request, *args, **kwargs):
+        test = get_object_or_404(Test, pk=self.kwargs.get('pk'))
+        data = json.loads(request.body)
+        skill_lvl = SkillLevel.objects.get(pk=data['id'])
+        code = skill_lvl.skill.code
+
+        for i in test.skill_level.all():
+            if skill_lvl == i:
+                test.skill_level.remove(i)
+                test.save()
+                return redirect('webapp:child_update_test', pk=test.pk)
+            if code == i.skill.code:
+                test.skill_level.remove(i)
+        test.skill_level.add(skill_lvl)
+        test.save()
+        return redirect('webapp:child_update_test', pk=test.pk)
