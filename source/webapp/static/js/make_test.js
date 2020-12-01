e2e022f4b1a20c8499a6ae88149d8ed9c5c3dddd
Emil <emilrakaev@gmail.com>
2020-12-01 18:22:19 +0600

    Реализовано проведение теста

---
+++ source/webapp/static/js/make_test.js
@@ -0,0 +1,74 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+async function makeRequest(url, method = 'GET', data = undefined) {
+    let opts = {method, headers: {}};
+
+    if (!csrfSafeMethod(method))
+        opts.headers['X-CSRFToken'] = getCookie('csrftoken');
+
+    if (data) {
+        opts.headers['Content-Type'] = 'application/json';
+        opts.body = JSON.stringify(data);
+    }
+
+    let response = await fetch(url, opts);
+
+    if (response.ok) {  // нормальный ответ
+        return response;
+    } else {            // ошибка
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+}
+
+let colorDict = {}
+
+async function choice_level(event) {
+    event.preventDefault();
+    let choice = event.target;
+    let id = choice.id;
+    let url = choice.href;
+    let code = document.getElementById('codes')
+    let row = choice.parentElement  //ряд
+    let colorCode = row.childNodes[1].textContent  //ключ(код)
+
+
+    colorDict[colorCode] = choice
+    Object.keys(colorDict).forEach(function (key) {
+        if (key === colorCode && colorDict[key].style.backgroundColor === 'lightgreen') {
+            colorDict[colorCode].style.backgroundColor = 'white'
+        }
+        else if(key === colorCode && colorDict[key].style.backgroundColor !== 'lightgreen'){
+            for (let i = 0; i < row.childElementCount - 1; i++) {
+                row.querySelectorAll('a')[i].style.backgroundColor = 'white'
+            }
+            colorDict[colorCode].style.backgroundColor = 'lightgreen'
+        }
+    });
+
+    try {
+        let response = await makeRequest(url, 'POST', {'id': id});
+    } catch (error) {
+        console.log(error);
+    }
+}
--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from webapp.views.child_test import TestResultView, TestsView
+from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.child import IndexView, ChildDetailView
 from webapp.views.program import ProgramDetailView, ProgramCreateView
 
@@ -11,11 +11,13 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
     path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
+    path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
     path('child/', include([
         path('<int:pk>/', include([
             path('', ChildDetailView.as_view(), name='child_view'),
             path('tests/', TestsView.as_view(), name='child_tests'),
             path('program/create/', ProgramCreateView.as_view(), name='program_create'),
+            path('create_test/', ChildMakeTestView.as_view(), name='child_create_test'),
         ])),
     ])),
 ]
