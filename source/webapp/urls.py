d530f2db02e33ff99824291a928449b585314a78
Artur <metis130383@gmail.com>
2020-12-12 02:15:07 +0600

    Реализован UpdateProgram (Добавления навыка  в  программу) реализованна  верстка + JS Испарвленны  не  доработки теперь после  добовления  програмы  ридерект идет на  добавления  навыка

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.child import IndexView, ChildDetailView
-from webapp.views.program import ProgramDetailView, ProgramCreateView
+from webapp.views.program import ProgramDetailView, ProgramCreateView,UpdateProgram
 
 app_name = 'webapp'
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
     path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
+    path('program/<int:pk>/update/', UpdateProgram.as_view(), name='update_program'),
     path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
     path('child/', include([
         path('<int:pk>/', include([
--- source/webapp/views/program.py
+++ source/webapp/views/program.py
@@ -1,10 +1,13 @@
-from django.shortcuts import get_object_or_404
+import json
+
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic.base import View, TemplateView
 
 from webapp.forms import ProgramForm
 from webapp.models import Program, SKILL_STATUS_OPEN, PROGRAM_STATUS_CLOSED, PROGRAM_STATUS_OPEN, Child, ProgramSkill, \
-    GOAL_STATUS_OPEN, ProrgamSkillGoal
+    GOAL_STATUS_OPEN, ProrgamSkillGoal, Test, Skill, SkillLevel
 
 
 class ProgramDetailView(DetailView):
@@ -36,6 +39,7 @@ class ProgramCreateView(CreateView):
     template_name = 'program/program_create.html'
     form_class = ProgramForm
 
+
     def form_valid(self, form):
         child = get_object_or_404(Child, pk=self.kwargs.get('pk'))
         program = form.save(commit=False)
@@ -47,5 +51,47 @@ class ProgramCreateView(CreateView):
     def get_success_url(self):
         child = get_object_or_404(Child, pk=self.kwargs.get('pk'))
         program = get_object_or_404(Program, pk=child.programs.last().pk)
-        return reverse('webapp:program_detail', kwargs={'pk': program.pk})
+        return reverse('webapp:update_program', kwargs={'pk': program.pk})
+
+
+class UpdateProgram(TemplateView):
+    template_name = 'program/program_add_skill.html'
+    model = Program
+
+    def get_context_data(self, **kwargs):
+        program = get_object_or_404(Program, pk=self.kwargs.get('pk'))
+        child = program.child
+        test = child.test.last()
+        context = super().get_context_data(**kwargs)
+        if test:
+            category_cod = self.request.GET.get('ABC')
+            context['skills'] = Skill.objects.filter(category__code=category_cod)
+            context['child_pk'] = test.child.pk
+            context['program'] = program
+            context['test'] = test
+            context['ABC'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                              'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+        else:
+            context['error'] = 'Нет ни одного проведенного теста'
+        return context
+
+    def post(self, request, *args, **kwargs):
+        program = get_object_or_404(Program, pk=self.kwargs.get('pk'))
+        data = json.loads(request.body)
+        skill_lvl = SkillLevel.objects.get(pk=data['id'])
+        program.skills.add(skill_lvl)
+        return redirect('webapp:update_program', pk=program.pk)
+        # code = skill_lvl.skill.code
+
+
+        # for i in program.program_skill.all():
+        #     if skill_lvl == i:
+        #         program.program_skill.remove(i)
+        #         program.save()
+        #         return redirect('webapp:update_program', pk=program.pk)
+        #     if code == i.skill.code:
+        #         program.program_skill.remove(i)
+        # program.program_skill.add(skill_lvl)
+        # program.save()
 
