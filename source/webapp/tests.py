916854d7f11cac140ace7c51fcb55d6b9ff47b09
alex <nuvorish81@mail.ru>
2020-12-23 20:44:59 +0600

    Доделан тест на проверку функции вывода результатов тестирования #47

--- source/webapp/tests.py
+++ source/webapp/tests.py
@@ -49,25 +49,21 @@ class ResultTestCompereTestCase(TestCase):
         cls.skill_code = 'A3'
         cls.category_code = 'A'
         skill_level = SkillLevel.objects.filter(skill__code=cls.skill_code)
-        first_skl = skill_level.first()
-        second_skl = skill_level.all()[1]
-        third_skl = skill_level.all()[2]
-        last_skl = skill_level.last()
-        print(first_skl)
-        print(second_skl)
-        print(third_skl)
-        print(last_skl)
+        cls.first_skl = skill_level.first()
+        cls.second_skl = skill_level.all()[1]
+        cls.third_skl = skill_level.all()[2]
+        cls.last_skl = skill_level.last()
         child = Child.objects.create(first_name='Test_Child', age=5, first_parent='Test_Parent')
         child.therapy.add(user)
         child.save()
         previous_test = Test.objects.create(child=child, therapist=user)
-        previous_test.skill_level.add(first_skl)
         previous_test.save()
         test = Test.objects.create(child=child, therapist=user, previus_test=previous_test)
-        test.skill_level.add(second_skl)
         test.save()
         cls.user = user
         cls.test = test
+        cls.previous_test = previous_test
+
 
     def setUp(self) -> None:
         self.client.login(username='admin1', password='admin1')
@@ -77,16 +73,63 @@ class ResultTestCompereTestCase(TestCase):
     def tearDown(self) -> None:
         self.client.logout()
 
-    def test_create_article_success(self):
+    def test__test_result_success(self):
+        self.previous_test.skill_level.add(self.first_skl)
+        self.test.skill_level.add(self.second_skl)
         self.assertEqual(self.response.status_code, 200)
         self.assertEqual(type(self.response), TemplateResponse)
 
-    def test_article_create_success_data(self):
+    def test_test_compere_success_data_1(self):
+        self.previous_test.skill_level.add(self.first_skl)
+        self.test.skill_level.add(self.second_skl)
         result = self.c.all_test(self.test.pk, category_cod=self.category_code, checkbox=1)
-        print(result)
         for skill in result:
             if self.skill_code != skill:
                 self.assertTrue(result[skill], 'none')
             else:
+                self.assertEqual(self.skill_code, skill)
                 self.assertNotEqual(result[skill], 'none')
+                self.assertIn('previous', result[skill])
+                self.assertIn('last', result[skill])
+                self.assertIn('max', result[skill])
+                self.assertIn('empty', result[skill])
+                self.assertIn('max_prev_lev', result[skill])
+                self.assertEqual(list(result[skill]['previous'])[0], self.first_skl.level)
+                self.assertEqual(list(result[skill]['last'])[0], self.second_skl.level)
+                self.assertEqual(list(result[skill]['empty'])[-1], self.last_skl.level)
+                self.assertEqual(list(result[skill]['empty'])[-1], result[skill]['max'])
+                self.assertTrue(list(result[skill]['last'])[0] > list(result[skill]['previous'])[0])
+                self.assertEqual(list(result[skill]['previous'])[0], result[skill]['max_prev_lev'])
 
+    def test_test_compere_success_data_2(self):
+        self.previous_test.skill_level.add(self.last_skl)
+        self.test.skill_level.add(self.second_skl)
+        result = self.c.all_test(self.test.pk, category_cod=self.category_code, checkbox=1)
+        for skill in result:
+            if self.skill_code != skill:
+                self.assertTrue(result[skill], 'none')
+            else:
+                self.assertEqual(self.skill_code, skill)
+                self.assertNotEqual(result[skill], 'none')
+                self.assertIn('previous', result[skill])
+                self.assertIn('last', result[skill])
+                self.assertIn('max', result[skill])
+                self.assertIn('empty', result[skill])
+                self.assertIn('max_prev_lev', result[skill])
+                self.assertEqual(list(result[skill]['previous']), [1, 2, 3, 4])
+                self.assertEqual(result[skill]['last'], 0)
+                self.assertEqual(list(result[skill]['empty']), [])
+                self.assertEqual(list(result[skill]['previous'])[-1], result[skill]['max_prev_lev'])
+                self.assertEqual(result[skill]['max'], result[skill]['max_prev_lev'])
+
+    def test_test_compere_success_data_3(self):
+        self.previous_test.skill_level.add(self.last_skl)
+        self.test.skill_level.add(self.second_skl)
+        result = self.c.all_test(self.test.pk, category_cod=self.category_code, checkbox='')
+        for skill in result:
+            self.assertNotEqual(result[skill], 'none')
+            self.assertIn('previous', result[skill])
+            self.assertIn('last', result[skill])
+            self.assertIn('max', result[skill])
+            self.assertIn('empty', result[skill])
+            self.assertIn('max_prev_lev', result[skill])
