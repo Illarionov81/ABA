dbc0abed6cd52abcf349cdb7426b77bb65d49717
artur <metis130383@gmail.com>
2020-12-29 03:29:43 +0600

    Реализовано удаление программы с протверждением тикет #49

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.child import IndexView, ChildDetailView
-from webapp.views.program import ProgramDetailView, ProgramCreateView,UpdateProgram
+from webapp.views.program import ProgramDetailView, ProgramCreateView,UpdateProgram, ProgrmDelete
 from webapp.views.session import SessionListView, SessionCreateView, SessionSkillCreateView
 
 app_name = 'webapp'
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
     path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
+    path('program/<int:pk>/delete/', ProgrmDelete.as_view(), name='program_delete'),
     path('program/<int:pk>/update/', UpdateProgram.as_view(), name='update_program'),
     path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
     path('session/<int:pk>/', SessionListView.as_view(), name='session_list'),
