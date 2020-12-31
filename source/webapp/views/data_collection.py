4b82becda53d82408217066f4f521a9e774c8391
alex <nuvorish81@mail.ru>
2020-12-31 15:58:54 +0600

    Изменил способ сортировки для навигации между навыками - теперь работает на много быстрее  #67

--- source/webapp/views/data_collection.py
+++ source/webapp/views/data_collection.py
@@ -13,11 +13,10 @@ class SessionDataCollectionView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        category_cod = self.request.GET.get('ABC')
+        category_code = self.request.GET.get('ABC')
         session = Session.objects.filter(program=self.object).last()
-        context['skills'] = Skill.objects.filter(category__code=category_cod)
-        # context['child_pk'] = test.child.pk
-        # context['test'] = test
+        context['child'] = self.object.child
+        context['category_code'] = category_code
         context['ABC'] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                           'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         context['session'] = session
