3ae9715e6f417818dfadff261a6d8fd777b7ed8d
artur <metis130383@gmail.com>
2021-01-05 03:16:39 +0600

    Отредактировал вывовд удаления в program_detail_view.html исправил ошибки в urls.py тикет #49

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from webapp.views import SessionDataCollectionView, DoneSelf, DoneWithHint, SessionAddGoalView, SessionCloseView
-from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.child import IndexView, ChildDetailView
-from webapp.views.program import ProgramDetailView, ProgramCreateView,UpdateProgram, ProgrmDelete, DeleteGoalView
+from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
+from webapp.views.program import ProgramDetailView, ProgramCreateView, UpdateProgram, ProgrmDelete, DeleteGoalView, \
+    DeleteCreteriaView, DeleteAddCreteriaView
 from webapp.views.session import SessionListView, SessionCreateView, SessionSkillCreateView
 
 app_name = 'webapp'
@@ -11,17 +11,14 @@ app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('done_self/<int:pk>/', DoneSelf.as_view(), name='done_self'),
-    path('done_with_hint/<int:pk>/', DoneWithHint.as_view(), name='done_with_hint'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
     path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
     path('program/<int:pk>/delete/', ProgrmDelete.as_view(), name='program_delete'),
     path('delete/goal/<int:pk>/', DeleteGoalView.as_view(), name='delete_goal'),
+    path('delete/<int:pk>/critaria', DeleteCreteriaView.as_view(), name='delete_creteria'),
+    path('delete/<int:pk>/add_critaria', DeleteAddCreteriaView.as_view(), name='delete_add_creteria'),
     path('program/<int:pk>/update/', UpdateProgram.as_view(), name='update_program'),
-    path('session/<int:pk>/close', SessionCloseView.as_view(), name='session_close'),
     path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
-    path('program/<int:pk>/add-goal/to-level/<int:level>/', SessionAddGoalView.as_view(), name='session_add_goal'),
-    path('program/<int:pk>/data-collection/', SessionDataCollectionView.as_view(), name='session_data_collection'),
     path('session/<int:pk>/', SessionListView.as_view(), name='session_list'),
     path('session/<int:pk>/create', SessionCreateView.as_view(), name='session_create'),
     path('session/<int:pk>/create/skill', SessionSkillCreateView.as_view(), name='session_prepear'),
