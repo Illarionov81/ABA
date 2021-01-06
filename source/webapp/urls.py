ef0849528d11f2c872f00ba1276d4e377555d6b7
artur <metis130383@gmail.com>
2021-01-07 03:44:14 +0600

    Востановил url... №3. #67

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
+from webapp.views import DoneSelf, DoneWithHint, SessionCloseView, SessionAddGoalView, SessionDataCollectionView
 from webapp.views.child import IndexView, ChildDetailView
 from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.program import ProgramDetailView, ProgramCreateView, UpdateProgram, ProgrmDelete, DeleteGoalView, \
@@ -11,6 +12,8 @@ app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('done_self/<int:pk>/', DoneSelf.as_view(), name='done_self'),
+    path('done_with_hint/<int:pk>/', DoneWithHint.as_view(), name='done_with_hint'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
     path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
     path('program/<int:pk>/delete/', ProgrmDelete.as_view(), name='program_delete'),
@@ -19,7 +22,13 @@ urlpatterns = [
     path('delete/<int:pk>/add_critaria', DeleteAddCreteriaView.as_view(), name='delete_add_creteria'),
     path('program/<int:pk>/update/', UpdateProgram.as_view(), name='update_program'),
     path('program/<int:p_pk>/remove/<int:s_pk>/', RemoveProgramView.as_view(), name='update_all_program'),
+    path('session/<int:pk>/close', SessionCloseView.as_view(), name='session_close'),
     path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
+    path('program/<int:pk>/add-goal/to-level/<int:level>/', SessionAddGoalView.as_view(), name='session_add_goal'),
+    path('program/<int:pk>/data-collection/', SessionDataCollectionView.as_view(), name='session_data_collection'),
+    path('session/<int:pk>/', SessionListView.as_view(), name='session_list'),
+    path('session/<int:pk>/create', SessionCreateView.as_view(), name='session_create'),
+    path('session/<int:pk>/create/skill', SessionSkillCreateView.as_view(), name='session_prepear'),
     path('session/<int:pk>/', SessionListView.as_view(), name='session_list'),
     path('session/<int:pk>/create', SessionCreateView.as_view(), name='session_create'),
     path('session/<int:pk>/create/skill', SessionSkillCreateView.as_view(), name='session_prepear'),
