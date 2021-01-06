19d26ce97daf866ab1a284a2e1230ba7bc151839
artur <metis130383@gmail.com>
2021-01-07 03:05:17 +0600

    Реализован js для редактирования и удаления критерикв, доп.критериев и цулей  в программе. Реализован вьюха для редактирования и удаления критерикв, доп.критериев и цулей  в программе Тикет #49

--- source/webapp/static/js/program_add_skill.js
+++ source/webapp/static/js/program_add_skill.js
@@ -41,6 +41,73 @@ async function makeRequest(url, method = 'GET', data = undefined) {
     }
 }
 
+async function update_program(event) {
+    event.preventDefault()
+    let id = event.target.getAttribute("data-id")
+    document.getElementById("add_creteria").value = ""
+    // const criteria = document.getElementById("criteria")
+    // if(criteria){
+    //     criteria.remove()
+    // }
+    let modal_body = document.getElementById("modal_body")
+    const main_goal = document.getElementById("add_goal")
+    let add_goal = [...(modal_body.getElementsByClassName("add_goal"))]
+    main_goal.value = ""
+            add_goal.forEach((el)=>{
+                if (el !== main_goal){
+                    el.remove()
+                }
+            })
+    let url = event.target.getAttribute("href");
+    const response = await makeRequest(url);
+    let update_pr = await response.json();
+    let add_creteria = document.getElementById("add_creteria")
+    add_creteria.value = update_pr.add_creteria
+    // const input_criteria = document.createElement("input")
+    // input_criteria.setAttribute("id", "criteria")
+    // input_criteria.value = update_pr.criteria
+    // add_creteria.before(input_criteria)
+    const add_new_goal = document.getElementById('add_new_goal')
+    add_new_goal.onclick = (e) => {
+        e.preventDefault()
+        const input = document.createElement("input")
+        input.setAttribute("class", "add_goal")
+        input.setAttribute("placeholder", "Добавить цель")
+        add_new_goal.after(input)
+    }
+    for(let i=0; i<update_pr.goals.length; i++){
+        const input = document.createElement("input")
+        input.setAttribute("class", "add_goal")
+        input.setAttribute("placeholder", "Добавить цель")
+        input.value = update_pr.goals[i]
+        add_new_goal.after(input)
+    }
+    $("#exampleModal").modal("show")
+    const button = document.getElementById('save')
+
+    button.onclick =  async (e) => {
+        e.preventDefault()
+
+        event.target.style.backgroundColor = 'red'
+        let add_creteria = document.getElementById("add_creteria")
+        let add_creteria_value = add_creteria.value
+        // let criteria = input_criteria.value
+        let add_goal = [...(modal_body.getElementsByClassName("add_goal"))]
+        let goals = add_goal.map( (el) => {
+                return el.value
+        })
+        try {
+            await makeRequest(url, 'POST', {'id': id, 'add_creteria': add_creteria_value, 'goals': goals});
+
+
+            $("#exampleModal").modal("hide")
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
 function add_new_input_goal(){
     const modal_body = document.getElementById("add_new_goal");
     const input = document.createElement("input")
@@ -51,6 +118,21 @@ function add_new_input_goal(){
 
 async function open_modal(event){
     event.preventDefault()
+    document.getElementById("add_creteria").value = ""
+    const criteria = document.getElementById("criteria")
+    if(criteria){
+        criteria.remove()
+    }
+    let modal_body = document.getElementById("modal_body")
+    const main_goal = document.getElementById("add_goal")
+    let add_goal = [...(modal_body.getElementsByClassName("add_goal"))]
+    main_goal.value = ""
+            add_goal.forEach((el)=>{
+                if (el !== main_goal){
+                    el.remove()
+                }
+            })
+
     $("#exampleModal").modal("show")
     let id = event.target.getAttribute("data-id")
     let url = event.target.getAttribute("href");
@@ -65,7 +147,7 @@ async function open_modal(event){
     }
     button.onclick =  async (e) => {
         e.preventDefault()
-        let modal_body = document.getElementById("modal_body")
+
         event.target.style.backgroundColor = 'red'
         let add_creteria = document.getElementById("add_creteria")
         let add_creteria_value = add_creteria.value
@@ -76,14 +158,7 @@ async function open_modal(event){
         try {
             await makeRequest(url, 'POST', {'id': id, 'add_creteria': add_creteria_value, 'goals': goals});
 
-            document.getElementById("add_creteria").value = ""
-            const main_goal = document.getElementById("add_goal")
-            main_goal.value = ""
-            add_goal.forEach((el)=>{
-                if (el !== main_goal){
-                    el.remove()
-                }
-            })
+
             $("#exampleModal").modal("hide")
         }
         catch (error) {
--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 
-from webapp.views import DoneSelf, DoneWithHint, SessionCloseView, SessionAddGoalView, SessionDataCollectionView
 from webapp.views.child import IndexView, ChildDetailView
 from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.program import ProgramDetailView, ProgramCreateView, UpdateProgram, ProgrmDelete, DeleteGoalView, \
-    DeleteCreteriaView, DeleteAddCreteriaView
+    DeleteCreteriaView, DeleteAddCreteriaView, RemoveProgramView
 from webapp.views.session import SessionListView, SessionCreateView, SessionSkillCreateView
 
 app_name = 'webapp'
@@ -12,22 +11,18 @@ app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('session/done_self/<int:pk>/', DoneSelf.as_view(), name='done_self'),
-    path('session/done_with_hint/<int:pk>/', DoneWithHint.as_view(), name='done_with_hint'),
-    path('session/<int:pk>/', SessionListView.as_view(), name='session_list'),
-    path('session/<int:pk>/close', SessionCloseView.as_view(), name='session_close'),
-    path('session/<int:pk>/create', SessionCreateView.as_view(), name='session_create'),
-    path('session/<int:pk>/create/skill', SessionSkillCreateView.as_view(), name='session_prepear'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
-    path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
     path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
     path('program/<int:pk>/delete/', ProgrmDelete.as_view(), name='program_delete'),
     path('delete/goal/<int:pk>/', DeleteGoalView.as_view(), name='delete_goal'),
     path('delete/<int:pk>/critaria', DeleteCreteriaView.as_view(), name='delete_creteria'),
     path('delete/<int:pk>/add_critaria', DeleteAddCreteriaView.as_view(), name='delete_add_creteria'),
     path('program/<int:pk>/update/', UpdateProgram.as_view(), name='update_program'),
-    path('program/<int:pk>/add-goal/to-level/<int:level>/', SessionAddGoalView.as_view(), name='session_add_goal'),
-    path('program/<int:pk>/data-collection/', SessionDataCollectionView.as_view(), name='session_data_collection'),
+    path('program/<int:p_pk>/remove/<int:s_pk>/', RemoveProgramView.as_view(), name='update_all_program'),
+    path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
+    path('session/<int:pk>/', SessionListView.as_view(), name='session_list'),
+    path('session/<int:pk>/create', SessionCreateView.as_view(), name='session_create'),
+    path('session/<int:pk>/create/skill', SessionSkillCreateView.as_view(), name='session_prepear'),
     path('child/', include([
         path('<int:pk>/', include([
             path('', ChildDetailView.as_view(), name='child_view'),
