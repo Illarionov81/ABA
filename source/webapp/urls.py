e8e67ebd430e97385e8427647ce8cc7f1ceab204
Emil <emilrakaev@gmail.com>
2021-01-07 23:09:32 +0600

    Подкорректировал url, добавил стиль к кнопке

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -4,20 +4,15 @@ from webapp.views import DoneSelf, DoneWithHint, SessionCloseView, SessionAddGoa
 from webapp.views.child import IndexView, ChildDetailView
 from webapp.views.child_test import TestResultView, TestsView, ChildTestUpdateView, ChildMakeTestView
 from webapp.views.program import ProgramDetailView, ProgramCreateView, UpdateProgram, ProgrmDelete, DeleteGoalView, \
-    DeleteCreteriaView, DeleteAddCreteriaView, RemoveProgramView
+    DeleteCreteriaView, DeleteAddCreteriaView
 from webapp.views.session import SessionListView, SessionCreateView, SessionSkillCreateView
+from webapp.views.session import SessionListView, SessionCreateView, SessionSkillCreateView, SessionAddSkill, \
+    SessionDeleteSkill
 
 app_name = 'webapp'
 
-
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('session/done_self/<int:pk>/', DoneSelf.as_view(), name='done_self'),
-    path('session/done_with_hint/<int:pk>/', DoneWithHint.as_view(), name='done_with_hint'),
-    path('session/<int:pk>/', SessionListView.as_view(), name='session_list'),
-    path('session/<int:pk>/close', SessionCloseView.as_view(), name='session_close'),
-    path('session/<int:pk>/create', SessionCreateView.as_view(), name='session_create'),
-    path('session/<int:pk>/create/skill', SessionSkillCreateView.as_view(), name='session_prepear'),
     path('test_result/<int:pk>/', TestResultView.as_view(), name='test_result'),
     path('update_test/<int:pk>/', ChildTestUpdateView.as_view(), name='child_update_test'),
     path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_detail'),
@@ -26,9 +21,20 @@ urlpatterns = [
     path('delete/<int:pk>/critaria', DeleteCreteriaView.as_view(), name='delete_creteria'),
     path('delete/<int:pk>/add_critaria', DeleteAddCreteriaView.as_view(), name='delete_add_creteria'),
     path('program/<int:pk>/update/', UpdateProgram.as_view(), name='update_program'),
-    path('program/<int:p_pk>/remove/<int:s_pk>/', RemoveProgramView.as_view(), name='update_all_program'),
     path('program/<int:pk>/add-goal/to-level/<int:level>/', SessionAddGoalView.as_view(), name='session_add_goal'),
     path('program/<int:pk>/data-collection/', SessionDataCollectionView.as_view(), name='session_data_collection'),
+    path('session', include([
+        path('done_self/<int:pk>/', DoneSelf.as_view(), name='done_self'),
+        path('done_with_hint/<int:pk>/', DoneWithHint.as_view(), name='done_with_hint'),
+        path('<int:pk>/', include([
+            path('', SessionListView.as_view(), name='session_list'),
+            path('close', SessionCloseView.as_view(), name='session_close'),
+            path('create', SessionCreateView.as_view(), name='session_create'),
+            path('create/skill', SessionSkillCreateView.as_view(), name='session_prepear'),
+            path('create/skill/add', SessionAddSkill.as_view(), name='add_skill'),
+            path('create/skill/delete', SessionDeleteSkill.as_view(), name='delete_skill'),
+        ]))
+    ])),
     path('child/', include([
         path('<int:pk>/', include([
             path('', ChildDetailView.as_view(), name='child_view'),
